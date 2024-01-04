c21a41750ca419fd1c743dde2c0e3e6ee361a3bb
temucode <magvan.temuulen@gmail.com>
2024-01-04 16:55:42 +0800

    django app part 3

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from django.template import loader
+
+from .models import Question
 
 
 def index(request):
@@ -22,3 +25,44 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+# Leave the rest of the views (detail, results, vote) unchanged
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged
+
+#import loader
+#def index(request):
+    #latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    #template = loader.get_template("polls/index.html")
+    #context = {
+        #"latest_question_list": latest_question_list,
+    #}
+    #return HttpResponse(template.render(context, request))
+#import loader
+
+#import render
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+#import render
+
+#import Http404
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+#Http404
+
+#import get_object_or_404
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+#import get_object_or_404
