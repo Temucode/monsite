60668cd8a8a0a1252b0908bec693a3a84f0f91eb
temucode <magvan.temuulen@gmail.com>
2024-01-05 11:12:56 +0800

    Django app, part 7. Customize the admin with form

--- monsite/settings.py
+++ monsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from pathlib import Path
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'monsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'polls', 'templates', 'polls')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
